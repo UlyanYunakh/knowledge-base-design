3bbec8f7184f8cd3401331bf76964f49e83f1be8
Ulyan Yunakh <44743429+UlyanYunakh@users.noreply.github.com>
2021-06-28 10:13:49 +0300

    Lab 3 (#1)
    * init
    
    * index form
    
    * db class and rel control
    
    * class copy
    
    * final
    
    * find

---
+++ Studio/Data/Relation.cs
@@ -0,0 +1,13 @@
+namespace Studio.Data
+{
+    public class Rel
+    {
+        public string Class1 { get; set; }
+        public string Class1Copy { get; set; }
+        public int Class1Id { get; set; }
+        public string Class2 { get; set; }
+        public string Class2Copy { get; set; }
+        public int Class2Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Studio/Startup.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Neo4j.Driver;
+
+namespace Studio
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            var uri = Configuration["NeO4jConnectionSettings:Server"];
+            var user = Configuration["NeO4jConnectionSettings:UserName"];
+            var pass = Configuration["NeO4jConnectionSettings:Password"];
+            services.AddSingleton(GraphDatabase.Driver(uri, AuthTokens.Basic(user, pass)));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
